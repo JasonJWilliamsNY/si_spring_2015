e4d3c257f24fd67a769a4e96f9cc374fd9401232
Jason Williams <JasonJWilliamsNY@gmail.com>
2015-03-10 13:27:40 -0400

    required commit before checkout

--- student_chi.py
+++ student_chi.py
@@ -13,36 +13,28 @@ from scipy import stats as sp
 observed_mnm = {}
 
 # Ask the user about each color and store the input
-''' M&M Colors:
-Brown, Blue, Red, Orange, Yellow, Green
-
-'''
-
 observed_mnm['Brown']=  float(raw_input('How many Brown M&Ms? \n'))
-observed_mnm['Blue']=   float(raw_input())
-
+observed_mnm['Blue']=   float(raw_input('How many Blue M&Ms? \n'))
+observed_mnm['Red']=    float(raw_input('How many Red M&Ms? \n'))
+observed_mnm['Orange']= float(raw_input('How many Orange M&Ms? \n'))
+observed_mnm['Yellow']= float(raw_input('How many Yellow M&Ms? \n'))
+observed_mnm['Green']=  float(raw_input('How many Green M&Ms? \n'))
 
 
 #turn the observed_mnm dictionary into a dataframe so we can do math
 
 data = pd.DataFrame.from_dict(observed_mnm, orient ='index')
 
-print data
-
-
 # add the name 'observed' to the dataframe
-#data.columns = ['observed'] 
+data.columns = ['observed'] 
 
 # convert the observations to percentages
 
-#data = data / data.sum()
+data = data / data.sum()
 
 # add an 'expected' column and fill in with 
 
-#data['expected'] = ''
-
-
-'''
+data['expected'] = ''
 data.expected['Blue'] = 0.24
 data.expected['Brown'] = 0.13
 data.expected['Green'] = 0.16
@@ -51,8 +43,7 @@ data.expected['Red'] = 0.13
 data.expected['Orange'] = 0.20
 
 print data
-'''
 
 # Use the stats package to do the ChiSquare test
-#result = sp.stats.chisquare(data.observation,data.expected,ddof=4)
-#print result
+result = sp.stats.chisquare(data.observed,data.expected,ddof=4)
+print result
